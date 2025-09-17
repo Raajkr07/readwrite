fe5ea9940ad6c2ea10aa5682daac101a64ab097d
RAJ KUMAR <rk8210032@gmail.com>
2025-09-17 23:59:55 +0530

    updated app jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,35 @@
+import React, { Suspense } from 'react';
+import { BrowserRouter } from 'react-router-dom';
+import { MantineProvider, createTheme } from '@mantine/core';
 import './styles/App.css'
+import LandingPage from './landing/LandingPage';
+
+
+const theme = createTheme({
+  focusRing: 'never',
+  fontFamily: 'poppins, sans-serif',
+  heading: { fontFamily: 'merriweather, serif' },
+  colors: {
+    primary: ['#49BBBD'],
+    dark: [ '#f6f6f6', '#e7e7e7', '#d1d1d1', '#b0b0b0', '#888888', '#6d6d6d', '#5d5d5d', '#4f4f4f', '#454545', '#3d3d3d', '#000000'
+    ],
+  },
+});
 
 function App() {
 
   return (
-    <div>
-      <p className='text-3xl font-bold '>
-        Hello world
-      </p>
-    </div>
+    <MantineProvider theme={theme} withNormalizeCSS withGlobalStyles>
+      <div >
+        <BrowserRouter>
+          <Suspense fallback={<div className="text-white text-center mt-20">
+            Just a Sec....
+            </div>}>
+            <LandingPage/>
+          </Suspense>
+        </BrowserRouter>
+      </div>
+    </MantineProvider>
   )
 }
 
