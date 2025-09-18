0e83c2c1eb32db7e21e27b24d3d92d2710c67791
RAJ KUMAR <rk8210032@gmail.com>
2025-09-18 23:28:32 +0530

    made landing page of this project

---
+++ frontend/src/landing/LandingPage.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import Header from '../components/header/Header';
+
+const LandingPage = () => {
+  return (
+    <div className="flex flex-col min-h-screen">
+      <Header />
+      <main className="flex-grow pt-[70px]">
+
+        <div >
+        </div>
+      </main>
+    </div>
+  )
+}
+
+export default LandingPage
